cdfeb302ea695470b663ad8d7b0671fdc2d44aae
Peter Esselius <peter.esselius@pagero.com>
2024-07-03 09:27:51 +0200

    README.md & formatting

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
-  description = "A very basic flake";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib.nixos) evalTest;
+      pkgs = builtins.trace "8. pkgs" import nixpkgs { system = "aarch64-darwin"; };
 
-  outputs = { self, nixpkgs }: 
-  let 
-    pkgs = builtins.trace "8. pkgs" import nixpkgs { system = "aarch64-darwin"; };
-    inherit (nixpkgs.lib.nixos) evalTest;
-    xx = builtins.trace "1. evaluating test" (evalTest { imports = [(builtins.trace "2. nixos test dot nix" ./nixos-test.nix)]; hostPkgs = builtins.trace "3. hostPkgs" pkgs; });
-  in {
-    x = builtins.trace "7. build.vm" xx.config.nodes.machine.system.build.vm;
-
-  };
+      xx = builtins.trace "1. evaluating test" (evalTest {
+        imports = [ (builtins.trace "2. nixos test dot nix" ./nixos-test.nix) ];
+        hostPkgs = builtins.trace "3. hostPkgs" pkgs;
+      });
+    in
+    {
+      x = builtins.trace "7. build.vm" xx.config.nodes.machine.system.build.vm;
+    };
 }
